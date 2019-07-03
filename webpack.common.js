46ead95ca76b83448364e2a261d8e48a88fc1423
simon <krafftsimon@gmail.com>
2019-07-02 21:15:03 -0600

    wip

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'js/[hash:8].bundle.js',
     path: path.resolve(__dirname, 'dist'),
